4e9345c2a13e4f552ada49c94a683d329dd1290f
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2019-04-11 00:18:32 -0300

    Add roteiros especiais e README

---
+++ RE4-01-RE-OrderStatisticsBubble-environment/src/main/java/orderStatistic/OrderStatisticsSelectionImpl.java
@@ -0,0 +1,27 @@
+package orderStatistic;
+
+public class OrderStatisticsSelectionImpl<T extends Comparable<T>> implements OrderStatistics<T> {
+
+	/**
+	 * Esta eh uma implementacao do calculo da estatistica de ordem seguindo a estrategia 
+	 * de usar o selection sem modificar o array original. Note que seu algoritmo vai 
+	 * apenas aplicar sucessivas vezes o selection ate encontrar a estatistica de ordem 
+	 * desejada sem modificar o array original. 
+	 * 
+	 * Restricoes:
+	 * - Preservar o array original, ou seja, nenhuma modificacao pode ser feita no 
+	 *   array original
+	 * - Nenhum array auxiliar deve ser criado e utilizado.
+	 * - Voce nao pode encontrar a k-esima estatistica de ordem por contagem de
+	 *   elementos maiores/menores, mas sim poraplciar sucessivas selecoes.
+	 * - Caso a estatistica de ordem nao exista no array, o algoritmo deve retornar null. 
+	 * - Considerar que k varia de 1 a N 
+	 * - Sugestao: o uso de recursao ajudara sua codificacao.
+	 */
+	@Override
+	public T getOrderStatistics(T[] array, int k) {
+		//TODO implement your code here
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+}
