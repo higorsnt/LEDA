b1007796bfb0a64e5707244ebbb9e796422a7500
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2019-05-06 07:15:42 -0300

    Correções TST

--- TST/src/FilaComArray.java
+++ TST/src/FilaComArray.java
@@ -5,78 +5,186 @@ public class FilaComArray {
     private int[] array;
     private int head;
     private int tail;
-    private int elements;
 
     public FilaComArray(int size) {
-        this.array = new int[size + 1];
-        this.head = 0;
-        this.elements = 0;
-        this.tail = 0;
+        this.array = new int[size];
+        this.head = -1;
+        this.tail = -1;
     }
 
     private boolean isFull(){
-        return (this.elements == this.array.length - 1);
+        return (this.tail == this.array.length - 1);
     }
 
     private boolean isEmpty() {
-        return (this.elements == 0);
+        return (this.tail == -1);
     }
 
-    public void add(int e) {
-        if (isFull()) System.out.println("full");
-        else {
-            this.array[this.tail++] = e;
-            this.elements++;
+    public void enqueue(int e) {
+        if (isFull())
+            System.out.println("full");
+        else if (isEmpty()){
+            this.head++;
+            this.array[++this.tail] = e;
+        } else {
+            this.array[++this.tail] = e;
         }
     }
 
-    public void element(){
-        if (isEmpty()) System.out.println("empty");
-        else System.out.println(this.array[this.head]);
+    public String head(){
+        String result = "empty";
+        if (!isEmpty())
+            result = "" + this.array[this.head];
+
+        return result;
     }
 
-    public void remove() {
-        if (isEmpty()) System.out.println("empty");
+    public void dequeue() {
+        if (isEmpty())
+            System.out.println("empty");
         else {
             this.head++;
-            this.elements--;
+            System.out.println(head + " " + tail);
         }
     }
 
-    public void print() {
-        if (isEmpty()) System.out.println("empty");
-        else {
-            String s = "";
-            for (int i = this.head; i < this.tail; i++) {
-                s += this.array[i] + " ";
-            }
+    public String print() {
+        String result = "empty";
 
-            System.out.println(s.trim());
+        if (!isEmpty()){
+            result = "";
+            for (int i = this.head; i <= this.tail; i++) {
+                result += this.array[i] + " ";
+            }
         }
+
+        return result.trim();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        FilaComArray f = new FilaComArray(sc.nextInt());
-        sc.nextLine();
-        String op;
+        int tamanho = Integer.parseInt(sc.nextLine());
+        FilaComArray fila = new FilaComArray(tamanho);
 
-        while (true){
-            op = sc.nextLine();
+        String[] entrada;
+        String operacao = "";
 
-            if (op.equalsIgnoreCase("print")){
-                f.print();
-            } else if (op.equalsIgnoreCase("remove")) {
-                f.remove();
-            } else if (op.equalsIgnoreCase("element")) {
-                f.element();
-            } else if (op.split(" ")[0].equalsIgnoreCase("add")) {
-                f.add(Integer.parseInt(op.split(" ")[1]));
-            } else if (op.equalsIgnoreCase("end")) {
-                break;
+        while (!operacao.equalsIgnoreCase("end")) {
+
+            entrada = sc.nextLine().split(" ");
+            operacao = entrada[0];
+
+            if (operacao.equalsIgnoreCase("add")) {
+                fila.enqueue(Integer.parseInt(entrada[1]));
+            } else if (operacao.equalsIgnoreCase("remove")) {
+                fila.dequeue();
+            } else if (operacao.equalsIgnoreCase("element")) {
+                System.out.println(fila.head());
+            } else if (operacao.equalsIgnoreCase("print")) {
+                System.out.println(fila.print());
             }
+
         }
+
+        sc.close();
     }
 
 }
+
+/*
+import java.util.Scanner;
+
+class FilaComArray {
+
+    private int[] array;
+    private int tail;
+
+    public FilaComArray(int size) {
+        this.array = new int[size];
+        this.tail = -1;
+    }
+
+    private boolean isFull(){
+        return (this.tail == this.array.length - 1);
+    }
+
+    private boolean isEmpty() {
+        return (this.tail == -1);
+    }
+
+    public void enqueue(int e) {
+        if (isFull())
+            System.out.println("full");
+        else
+            this.array[++this.tail] = e;
+    }
+
+    public String head(){
+        String result = "empty";
+        if (!isEmpty())
+            result = "" + this.array[0];
+
+        return result;
+    }
+
+    public void dequeue() {
+        if (isEmpty())
+            System.out.println("empty");
+        else {
+            shiftLeft();
+            this.tail--;
+        }
+    }
+
+    private void shiftLeft() {
+        for (int i = 0; i < this.tail; i++) {
+            this.array[i] = this.array[i + 1];
+        }
+    }
+
+    public String print() {
+        String result = "empty";
+
+        if (!isEmpty()) {
+            result = "";
+            for (int i = 0; i <= this.tail; i++) {
+                result += this.array[i] + " ";
+            }
+        }
+
+        return result.trim();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tamanho = Integer.parseInt(sc.nextLine());
+        FilaComArray fila = new FilaComArray(tamanho);
+
+        String[] entrada;
+        String operacao = "";
+
+        while (!operacao.equalsIgnoreCase("end")) {
+
+            entrada = sc.nextLine().split(" ");
+            operacao = entrada[0];
+
+            if (operacao.equalsIgnoreCase("add")) {
+                fila.enqueue(Integer.parseInt(entrada[1]));
+            } else if (operacao.equalsIgnoreCase("remove")) {
+                fila.dequeue();
+            } else if (operacao.equalsIgnoreCase("element")) {
+                System.out.println(fila.head());
+            } else if (operacao.equalsIgnoreCase("print")) {
+                System.out.println(fila.print());
+            }
+
+        }
+
+        sc.close();
+    }
+
+}
+
+ */
