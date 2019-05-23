7174e56086f42ad711cf023b3f1acf6fc41fba08
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2019-05-23 14:29:52 -0300

    R12

---
+++ R12-01-Rot-BST-Comparator-Sorting-environment/src/main/java/adt/bst/extended/SortComparatorBSTImpl.java
@@ -0,0 +1,45 @@
+package adt.bst.extended;
+
+import java.util.Comparator;
+
+import adt.bst.BSTImpl;
+
+/**
+ * Implementacao de SortComparatorBST, uma BST que usa um comparator interno em suas funcionalidades
+ * e possui um metodo de ordenar um array dado como parametro, retornando o resultado do percurso
+ * desejado que produz o array ordenado. 
+ * 
+ * @author Adalberto
+ *
+ * @param <T>
+ */
+public class SortComparatorBSTImpl<T extends Comparable<T>> extends BSTImpl<T> implements SortComparatorBST<T> {
+
+	private Comparator<T> comparator;
+	
+	public SortComparatorBSTImpl(Comparator<T> comparator) {
+		super();
+		this.comparator = comparator;
+	}
+
+	@Override
+	public T[] sort(T[] array) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T[] reverseOrder() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	public Comparator<T> getComparator() {
+		return comparator;
+	}
+
+	public void setComparator(Comparator<T> comparator) {
+		this.comparator = comparator;
+	}
+	
+}
